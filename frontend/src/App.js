9287ed8f7f280769bc199acde7fc2950b31ca879
dotuanminh <78138604+dotuanminh@users.noreply.github.com>
2023-12-18 16:36:25 +0000

    Commit frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './components/header/Header';
+import Home from './pages/home/home';
+import MovieList from './components/movieList/movieList';
+import Movie from './pages/movieDetail/movie';
+import RecommendationPage from './pages/recommendationPage/RecommendationPage';  // Make sure to import the correct path for RecommendationPage
+
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<Movie />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/movies/recommendation" element={<RecommendationPage />} />  {/* Corrected import */}
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
